6cfef11418989b007ec4d1fbf418a2a68ef84f3d
dipierrog <dipierrog@gilmour.org>
2016-11-02 10:11:01 -0400

    adding code fixes after seeing Mr.A's video

--- student.py
+++ student.py
@@ -7,19 +7,25 @@ from gopigo import *
 This class INHERITS your teacher's Pigo class. That means Mr. A can continue to
 improve the parent class and it won't overwrite your work.
 '''
-
+## TODO from the board in class: Calibrate, Cruise, Turn Track, Turn options, other
+## TODO add some more class variables
+## TODO put speed for left and right motors in student.py at the top with other variables
+    ## this helps so it doesn't veer off to the side
+## TODO replace calibrate in student with set speed method from pigo
 
 class GoPiggy(pigo.Pigo):
     # CUSTOM INSTANCE VARIABLES GO HERE. You get the empty self.scan array from Pigo
     # You may want to add a variable to store your default speed
     MIDPOINT = 89
     STOP_DIST = 30
+    RIGHT_SPEED = 200
+    LEFT_SPEED = 200
 
     # CONSTRUCTOR
     def __init__(self):
-        print("Piggy has be instantiated!")
-        # this method makes sure Piggy is looking forward
-        self.calibrate()
+        print("Piggy has been instantiated!")
+        ## set speed method, right and left speeds are loaded here
+        self.setSpeed(self.LEFT_SPEED, self.RIGHT_SPEED)
         # let's use an event-driven model, make a handler of sorts to listen for "events"
         while True:
             self.stop()
@@ -33,6 +39,7 @@ class GoPiggy(pigo.Pigo):
                 "2": ("Rotate", self.rotate),
                 "3": ("Dance", self.dance),
                 "4": ("Calibrate servo", self.calibrate),
+                ## does this still calibrate servo?
                 "q": ("Quit", quit)
                 }
         # loop and print the menu...
@@ -92,7 +99,8 @@ class GoPiggy(pigo.Pigo):
             elif answer == "right":
                 self.encR(5)
         ### TODO: how to change how much it turns when it changes directions
-                #TODO: add the test drive code and fix speed
+        ### TODO:start figuring out how many degrees it turns left or right
+            ## figure out how to get robot to turn without a set number?
 
 ####################################################
 ############### STATIC FUNCTIONS
