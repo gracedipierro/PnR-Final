f16edcf7f10a8a84dea7e8e5a0b085046a82880f
dipierrog <dipierrog@gilmour.org>
2016-11-07 10:41:39 -0500

    trying a new turn test

--- turntest.py
+++ turntest.py
@@ -1,16 +1,16 @@
 from gopigo import *
 print("Hi grace")
 while True:
-    turn = int(input("How much would you like to turn?"))
+    time = float(input("How much would you like to turn?"))
     rspeed = int(input("What would you like for your right speed?"))
     lspeed = int(input("What would you like for your left speed?"))
 
     set_left_speed(lspeed)
     set_right_speed(rspeed)
-    print(enc_tgt(1, 1, turn))
+    #print(enc_tgt(1, 1, turn))
     right_rot()
 
-    input("Press any key to stop")
-    #time.sleep(5)
+    time.sleep(time)
+
     stop()
     ##left_rot(turn)
